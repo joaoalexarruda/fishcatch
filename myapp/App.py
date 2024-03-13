eb378a439f0a5425cee9c9841f6561d8ef78a37f
Joao Alex <97920941+joaoalexarruda@users.noreply.github.com>
2024-03-13 07:41:35 -0300

    myapp

--- myapp/App.py
+++ myapp/App.py
@@ -2,16 +2,13 @@ import streamlit as st
 
 
 def app():
-    st.set_page_config(
-        page_title="Fishcatch",
-        page_icon="🐟",
-    )
+    st.set_page_config(page_title="Fishcatch", page_icon="🐟")
 
     col1, col2 = st.columns(2)
 
     with col1:
         st.title('Fishcatch: Predicting the width of a fish based on its features.')
-        
+
     with col2:
         st.image('https://imgur.com/23DiEOf.png', width=140)
 
