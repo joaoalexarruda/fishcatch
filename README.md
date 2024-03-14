01663b9ff7de4b13b82db5c1ec9cc6fc45120597
Joao Alex <97920941+joaoalexarruda@users.noreply.github.com>
2024-03-14 10:33:59 -0300

    separated the tasks into different notebooks

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # Fishcatch
 
+by [joaoalexarruda](https://github.com/joaoalexarruda)
+
 This repository contains the 'fishcatch' dataset. The dataset is part of an assignment for the post graduation course ['Data Science and Digital Transformation'](https://www.ipportalegre.pt/pt/oferta-formativa/pos-graduacao-data-science-and-digital-transformation) at the Instituto Politécnico de Portalegre (IPP).
 
 The goal is to apply machine learning techniques to the dataset and to evaluate the results. In this case, regression techniques such as linear and polynomial regression will be used to predict the width of the fish based on the other variables.
---
+++ my_functions/plot_regression.py
@@ -0,0 +1,22 @@
+import matplotlib.pyplot as plt
+
+
+def plot_regression(y_test, y_pred, title: str, regression_type: str = 'Linear'):
+    """Plots the actual width vs predicted width using a scatter plot.
+
+    Args:
+        y_test (array): y_test got from train_test_split.
+        y_pred (array): y_pred got from the model.
+        title (str): Title of the plot.
+        regression_type (str, optional): Type of regression. Defaults to 'Linear'.
+    """
+
+    fig, ax = plt.subplots(figsize=(10, 5), dpi=1080)
+    ax.scatter(y_test, y_pred, color='blue', label='Predicted Width')
+    ax.plot(y_test, y_test, color='red', label='Actual Width')
+    ax.set_xlabel('Actual Width')
+    ax.set_ylabel('Predicted Width')
+    ax.set_title(f'{regression_type} Regression: {title}')
+    ax.grid(True, linestyle='--', alpha=0.6)
+    ax.legend()
+    plt.show()
